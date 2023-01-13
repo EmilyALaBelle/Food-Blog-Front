6a56b0d48e9bbb5b4aa1a2d4d8c4ab5f9880f329
Emily LaBelle <emilylabelle8@gmail.com>
2023-01-12 23:48:40 -0500

    added universal styles and imported fonts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { AboutUs, CTAApps, CTADessert, Footer, Hero, LatestPost, Navbar, RecipeSlider } from './'
+import { AboutUs, CTAApps, CTADessert, Footer, Hero, LatestPost, Navbar, RecipeSlider } from './components'
 
 const App = () => (
     <div>
