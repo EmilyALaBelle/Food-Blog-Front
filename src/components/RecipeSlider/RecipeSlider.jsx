b2fd74ccd75186b0d0b1fb86ee937e2bff982925
Emily LaBelle <emilylabelle8@gmail.com>
2023-02-02 14:15:58 -0500

    added data and set up for slider

--- src/components/RecipeSlider/RecipeSlider.jsx
+++ src/components/RecipeSlider/RecipeSlider.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import {BsArrowLeftShort, BsArrowRightShort} from 'react-icons/bs'
+import {images} from '../../constants'
+import './RecipeSlider.css'
+
+const sliderImages = [images.hummus, images.eggTeaSandwich, images.southwestSalad]
 
 const RecipeSlider = () => {
+
+const scrollRef = React.useRef(null)
+
+const scroll = (direction) => {
+  const {current} = scrollRef;
+
+  if(direction === 'left') {
+    current.scrollLeft -= 300;
+  } else {
+    current.scrollLeft += 300
+  }
+}
+
   return (
-    <div>RecipeSlider</div>
+    <div className='app__slider'>
+      <div className='app__gallery-contents'>
+        <div className='app__gallery-contents_container'> 
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/constants/recipeCard.js
@@ -0,0 +1,30 @@
+import images from './images';
+
+const posts = [
+  {
+    imgUrl: images.hummus,
+    title: "Basil Hummus",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia pariatur culpa, esse nihil soluta totam aliquam nesciunt optio! Officiis, quibusdam. Quia sed repellat explicabo ipsam ea, aliquid hic ratione laudantium.",
+    user: "HummusGal1230",
+    date: "1-27-23",
+    cta: "Continue Reading ->",
+  },
+  {
+    imgUrl: images.eggTeaSandwich,
+    title: "Spinach and Egg Tea Sandwich",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia pariatur culpa, esse nihil soluta totam aliquam nesciunt optio! Officiis, quibusdam. Quia sed repellat explicabo ipsam ea, aliquid hic ratione laudantium.",
+    user: "FancyLady65",
+    date: "3-12-22",
+    cta: "Continue Reading ->",
+  },
+  {
+    imgUrl: images.southwestSalad,
+    title: "South West Salad",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia pariatur culpa, esse nihil soluta totam aliquam nesciunt optio! Officiis, quibusdam. Quia sed repellat explicabo ipsam ea, aliquid hic ratione laudantium.",
+    user: "HealthyHappy50",
+    date: "6-03-22",
+    cta: "Continue Reading ->",
+  },
+]
+
+export default {posts}
